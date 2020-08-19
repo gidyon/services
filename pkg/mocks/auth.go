7c9ce35cc7609b0589c236dae2c1bdbed004de98
gidyon <jessegitaka@jesses-MacBook-Air.local>
2020-08-19 14:19:59 +0300

    added AuthenticateRequestV2 to auth mock

--- pkg/mocks/auth.go
+++ pkg/mocks/auth.go
@@ -16,18 +16,20 @@ var AuthAPI = &mocks.AuthAPIMock{}
 
 func init() {
 	AuthAPI.On("AuthenticateRequest", mock.Anything).
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
+	AuthAPI.On("AuthenticateRequestV2", mock.Anything).
 		Return(nil)
 	AuthAPI.On("AuthorizeActor", mock.Anything, mock.Anything).
-		Return(&auth.Payload{}, nil)
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
 	AuthAPI.On("AuthorizeGroup",
 		mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return(&auth.Payload{}, nil)
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
 	AuthAPI.On("AuthorizeStrict",
 		mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return(&auth.Payload{}, nil)
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
 	AuthAPI.On("AuthorizeActorOrGroup",
 		mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return(&auth.Payload{}, nil)
+		Return(&auth.Payload{Group: auth.AdminGroup()}, nil)
 
 	AuthAPI.On("GenToken", mock.Anything, mock.Anything, mock.Anything).
 		Return("token", nil)
