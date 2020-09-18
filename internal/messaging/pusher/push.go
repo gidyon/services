741b568ade63be2ac0e758a1454788d3064b6a99
gidyon <gideonhacer@gmail.com>
2020-09-18 11:38:09 +0300

    updated gorm dep to use v2

--- internal/messaging/pusher/push.go
+++ internal/messaging/pusher/push.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/appleboy/go-fcm"
 	"github.com/golang/protobuf/ptypes/empty"
+	"gorm.io/gorm"
 
 	push "github.com/gidyon/services/pkg/api/messaging/pusher"
 	"github.com/gidyon/services/pkg/auth"
 	"github.com/gidyon/services/pkg/utils/errs"
-	"github.com/jinzhu/gorm"
 	"google.golang.org/grpc/grpclog"
 )
 
