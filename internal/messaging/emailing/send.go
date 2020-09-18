76ccdf6149d0da99b7ce4b3f8b57abb9228202ff
gidyon <gideonhacer@gmail.com>
2020-09-18 10:19:41 +0300

    support for attachemnts

--- internal/messaging/emailing/send.go
+++ internal/messaging/emailing/send.go
@@ -1,7 +1,10 @@
 package emailing
 
 import (
+	"io/ioutil"
+
 	"github.com/gidyon/services/pkg/api/messaging/emailing"
+	"github.com/gidyon/services/pkg/utils/errs"
 	"gopkg.in/gomail.v2"
 )
 
@@ -11,6 +14,21 @@ func (api *emailingAPIServer) sendEmail(email *emailing.Email) error {
 	m.SetHeader("To", email.Destinations...)
 	m.SetHeader("Subject", email.Subject)
 	m.SetBody(email.BodyContentType, email.Body)
+
+	var err error
+
+	// Create files
+	for _, attachment := range email.Attachments {
+		err = ioutil.WriteFile(attachment.Filename, attachment.Data, 0666)
+		if err != nil {
+			return errs.WriteFailed(err)
+		}
+	}
+
+	// Send attachements
+	for _, attachment := range email.Attachments {
+		m.Attach(attachment.Filename, gomail.Rename(attachment.FilenameOverride))
+	}
 
 	return api.dialer.DialAndSend(m)
 }
