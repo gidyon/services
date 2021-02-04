c5ca432e109d85f5a24b951cd8d3c86f0df9a27a
gidyon <gideon.ngk@gmail.com>
2021-02-04 09:44:37 +0300

    added label field in model

--- internal/channel/model.go
+++ internal/channel/model.go
@@ -11,7 +11,8 @@ const channelsTable = "channels"
 
 // Channel is a bulk channel
 type Channel struct {
-	Title       string `gorm:"type:varchar(50);unique;not null"`
+	Title       string `gorm:"index;type:varchar(50);unique;not null"`
+	Label       string `gorm:"index;type:varchar(50)"`
 	Description string `gorm:"type:text(500);not null"`
 	OwnerID     string `gorm:"type:varchar(50);not null"`
 	Subscribers int32  `gorm:"type:int(10);not null"`
@@ -28,6 +29,7 @@ func GetChannelPB(channelDB *Channel) (*channel.Channel, error) {
 	channelPB := &channel.Channel{
 		Id:                fmt.Sprint(channelDB.ID),
 		Title:             channelDB.Title,
+		Label:             channelDB.Label,
 		Description:       channelDB.Description,
 		OwnerId:           channelDB.OwnerID,
 		CreateTimeSeconds: int32(channelDB.CreatedAt.Unix()),
@@ -40,6 +42,7 @@ func GetChannelPB(channelDB *Channel) (*channel.Channel, error) {
 func GetChannelDB(channelPB *channel.Channel) (*Channel, error) {
 	channelDB := &Channel{
 		Title:       channelPB.Title,
+		Label:       channelPB.Label,
 		Description: channelPB.Description,
 		OwnerID:     channelPB.OwnerId,
 		Subscribers: channelPB.Subscribers,
