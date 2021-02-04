ec6f5c2b3b24a69d7293ddd682ae8af00e7c383d
gidyon <gideon.ngk@gmail.com>
2021-02-04 09:52:03 +0300

    updated channel API mocks

--- pkg/mocks/mocks/ChannelAPIClientMock.go
+++ pkg/mocks/mocks/ChannelAPIClientMock.go
@@ -199,6 +199,36 @@ func (_m *ChannelAPIClientMock) ListChannels(ctx context.Context, in *channel.Li
 	return r0, r1
 }
 
+// SearchChannels provides a mock function with given fields: ctx, in, opts
+func (_m *ChannelAPIClientMock) SearchChannels(ctx context.Context, in *channel.SearchChannelsRequest, opts ...grpc.CallOption) (*channel.ListChannelsResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *channel.ListChannelsResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *channel.SearchChannelsRequest, ...grpc.CallOption) *channel.ListChannelsResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*channel.ListChannelsResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *channel.SearchChannelsRequest, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // UpdateChannel provides a mock function with given fields: ctx, in, opts
 func (_m *ChannelAPIClientMock) UpdateChannel(ctx context.Context, in *channel.UpdateChannelRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	_va := make([]interface{}, len(opts))
