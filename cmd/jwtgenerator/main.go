2d2d719536ea63e561ca0ae75557a85a294f919f
gidyon <gideon.ngk@gmail.com>
2021-02-17 08:41:14 +0300

    program to generate SUPER_ADMIN jwt

---
+++ cmd/jwtgenerator/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/gidyon/micro/utils/errs"
+	"github.com/gidyon/micro/v2/pkg/middleware/grpc/auth"
+)
+
+var (
+	jwtKey = flag.String("jwt-key", "", "Key to use for signing JWT")
+)
+
+func main() {
+	flag.Parse()
+
+	jwtKey2 := []byte(*jwtKey)
+
+	// Authentication API
+	authAPI, err := auth.NewAPI(&auth.Options{
+		SigningKey: jwtKey2,
+		Issuer:     "Accounts API",
+		Audience:   "accounts",
+	})
+	errs.Panic(err)
+
+	// Generate jwt token
+	token, err := authAPI.GenToken(context.Background(), &auth.Payload{
+		ID:           "xxxxxx",
+		ProjectID:    "xxxxxx",
+		Names:        "xxxxxx",
+		PhoneNumber:  "xxxxxx",
+		EmailAddress: "xxxxxx",
+		Group:        auth.DefaultSuperAdminGroup(),
+	}, time.Now().Add(100*24*time.Hour))
+	errs.Panic(err)
+
+	fmt.Println(token)
+}
